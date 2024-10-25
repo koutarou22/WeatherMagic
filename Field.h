17fcdc94891f676c00c04e4bbe418d3f142049f4
直人 渡辺 <230080@jc-21.jp>
2024-10-25 18:25:15 +0900

    クリアでMPを取得できないバグ修正&文字化けを消す

--- Field.h
+++ Field.h
@@ -2,9 +2,9 @@
 #include "Engine/GameObject.h"
 
 /// <summary>
-/// 地面（ステージ）
-/// ※ステージ２を作る予定だったが、うまくいかなかったので修正
-/// </summary>
+/// 
+/// 
+/// /// </summary>
 class Field : public GameObject
 {
 public:
@@ -15,39 +15,39 @@ public:
 	void Draw() override;
 
 	/// <summary>
-	/// 右側の点が当たっているか調べる
+	/// 
 	/// </summary>
-	/// <param name="x">X座標</param>
-	/// <param name="y">Y座標</param>
-	/// <returns>めり込んだ量(ドット)</returns>
+	/// <param name="x">�E/param>
+	/// <param name="y">�E/param>
+	/// <returns>�E</returns>
 	int CollisionRight(int x, int y);
 	/// <summary>
-	/// 下の点が当たっているか調べる
+	///
 	/// </summary>
-	/// <param name="x">X座標</param>
-	/// <param name="y">Y座標</param>
-	/// <returns>めり込んだ量（ドット）</returns>
+	/// <param name="x">X�E/param>
+	/// <param name="y">Y�E/param>
+	/// <returns>�E�E/returns>
 	int CollisionDown(int x, int y);
 	/// <summary>
-	/// 左の点が当たっているか調べる
+	/// 
 	/// </summary>
-	/// <param name="x">x座標</param>
-	/// <param name="y">y座標</param>
-	/// <returns>めり込んだ量</returns>
+	/// <param name="x">x�E/param>
+	/// <param name="y">y�E/param>
+	/// <returns>�E/returns>
 	int CollisionLeft(int x, int y);
 
 	/// <summary>
-	/// 上の点があたっているか調べる
+	/// 
 	/// </summary>
-	/// <param name="x">x座標</param>
-	/// <param name="y">y座標</param>
-	/// <returns>めり込んだ量</returns>
+	/// <param name="x">x�E/param>
+	/// <param name="y">y�E/param>
+	/// <returns>�E/returns>
 	int CollisionUp(int x, int y);
 	bool IsWallBlock(int x, int y);
 	bool IsHitClear(int x, int y);
 
 	/// <summary>
-	/// ステージロード用
+	///
 	/// </summary>
 	/// <param name="StageNumber"></param>
 	void LoadStage(int StageNumber);
@@ -62,8 +62,7 @@ private:
 	int width;
 	int height;
   
-  int NowStage_ = 0;//現在のステージ番号を保管する用
-	int NowStage_;//現在のステージ番号を保管する用
+	int NowStage_;//���̃X�e�[�W�̔ԍ�
 	int hBackGroundDark_;
 
 	int padAnalogInput;//xbox�̓��͂��󂯎��
