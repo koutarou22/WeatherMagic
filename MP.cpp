b743530f8189c3a56cb81245de753f1a0a3570f0
幹人 永澤 <230413@jc-21.jp>
2024-12-09 13:17:23 +0900

    フレームを黒くした

--- MP.cpp
+++ MP.cpp
@@ -37,13 +37,14 @@ void MP::Draw()
 {
 	Player* player = GetParent()->FindGameObject<Player>();
 	//DrawGraph(-265, 50, hMPBar_, TRUE);
+	DrawGraph(-270, 58, hMPFrame_, TRUE);
 	if (player != nullptr && player->GetMp() > 0)
 	{
 		
 		DrawExtendGraph(trs.position_.x, trs.position_.y,
 		240*trs.scale_.x,trs.position_.y+60*trs.scale_.y, hMPBar_, TRUE);
 	}
-	DrawGraph(-265, 60, hMPFrame_, TRUE);
+	
 }
 
 void MP::Release()
