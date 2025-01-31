672a051f8521c2313dd05b3265713aaad7cfc980
光太郎 佐藤 <230011@jc-21.jp>
2025-01-31 11:50:57 +0900

    着地アニメーションの更新

--- LandingEffect.h
+++ LandingEffect.h
@@ -3,23 +3,36 @@
 /// <summary>
 /// 着地したときに起きる砂埃的なアレ
 /// </summary>
-class LandingEffect :public GameObject
-{
-	int hImage_;
-	int animeFrame;
-	int FrameCounter;
-	int eraseCounter;
+class LandingEffect : public GameObject {
 public:
+    LandingEffect(GameObject* parent);
+    ~LandingEffect();
 
-	LandingEffect(GameObject* parent);
-	~LandingEffect();
+    void Update() override;
+    void Draw() override;
+    void SetPosition(int x, int y);
+    /// <summary>
+    /// 生きてた時の処理
+    /// </summary>
+    void Activate();
 
-	//更新
-	void Update() override;
-	//描画
-	void Draw() override;
-	//開放
-	void Release() override;
+    /// <summary>
+    /// 死んだときの処理
+    /// </summary>
+    void Deactivate();
 
-	void SetPosition(int x, int y);
+    /// <summary>
+    /// 生きてるか返す
+    /// </summary>
+    /// <returns></returns>
+    bool IsActive() const;
+
+private:
+    static int hImage_;
+    int animeFrame;
+    int FrameCounter;
+    int eraseCounter;
+    bool isActive_; // アクティブ状態を管理するフラグ
+
+    void Release();
 };
